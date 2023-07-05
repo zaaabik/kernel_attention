39302c884d6b0a8aa15eebcdbc11b13023cf91fb
Zabolotnyi_Artem <zaaabik@gmail.com>
2023-07-05 15:41:37 +0300

    add f1 metrics and keep accuracy

--- src/models/token_classification_module.py
+++ src/models/token_classification_module.py
@@ -3,7 +3,7 @@ from typing import Any
 import torch
 from lightning import LightningModule
 from torchmetrics import MaxMetric, MeanMetric
-from torchmetrics import Accuracy
+from torchmetrics import Accuracy, F1Score
 
 
 class TokenClassificationModule(LightningModule):
@@ -44,6 +44,10 @@ class TokenClassificationModule(LightningModule):
         self.val_acc = Accuracy(task="multiclass", num_classes=self.hparams.num_classes, ignore_index=-100)
         self.test_acc = Accuracy(task="multiclass", num_classes=self.hparams.num_classes, ignore_index=-100)
 
+        self.train_f1 = Accuracy(task="multiclass", num_classes=self.hparams.num_classes, ignore_index=-100)
+        self.val_f1 = Accuracy(task="multiclass", num_classes=self.hparams.num_classes, ignore_index=-100)
+        self.test_f1 = Accuracy(task="multiclass", num_classes=self.hparams.num_classes, ignore_index=-100)
+
         # for averaging loss across batches
         self.train_loss = MeanMetric()
         self.val_loss = MeanMetric()
@@ -73,8 +77,10 @@ class TokenClassificationModule(LightningModule):
         loss, preds, targets = self.model_step(batch)
         self.train_loss(loss)
         self.train_acc(preds, targets)
+        self.train_f1(preds, targets)
         self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/f1", self.train_f1, on_step=False, on_epoch=True, prog_bar=True)
 
         return loss
 
@@ -87,8 +93,10 @@ class TokenClassificationModule(LightningModule):
         # update and log metrics
         self.val_loss(loss)
         self.val_acc(preds, targets)
+        self.val_f1(preds, targets)
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val/f1", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/f1", self.val_f1, on_step=False, on_epoch=True, prog_bar=True)
 
     def on_validation_epoch_end(self):
         acc = self.val_acc.compute()  # get current val acc
@@ -103,8 +111,10 @@ class TokenClassificationModule(LightningModule):
         # update and log metrics
         self.test_loss(loss)
         self.test_acc(preds, targets)
+        self.test_f1(preds, targets)
         self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("test/f1", self.test_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/acc", self.test_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/f1", self.test_f1, on_step=False, on_epoch=True, prog_bar=True)
 
     def on_test_epoch_end(self):
         pass
