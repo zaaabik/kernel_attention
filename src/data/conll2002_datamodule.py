deeb348a1270cbd00595acce3acf6c92d254fccf
Zabolotnyi_Artem <zaaabik@gmail.com>
2023-07-11 22:40:06 +0300

    back to tokenize_and_align_labels :)))

--- src/data/conll2002_datamodule.py
+++ src/data/conll2002_datamodule.py
@@ -161,28 +161,6 @@ class CoNLL2002DataModule(LightningDataModule):
         """Things to do when loading checkpoint."""
         pass
 
-    # def align_labels_with_tokens(self, labels, word_ids):
-    #     new_labels = []
-    #     current_word = None
-    #     for word_id in word_ids:
-    #         if word_id != current_word:
-    #             # Start of a new word!
-    #             current_word = word_id
-    #             label = -100 if word_id is None else labels[word_id]
-    #             new_labels.append(label)
-    #         elif word_id is None:
-    #             # Special token
-    #             new_labels.append(-100)
-    #         else:
-    #             # Same word as previous token
-    #             label = labels[word_id]
-    #             # If the label is B-XXX we change it to I-XXX
-    #             if label % 2 == 1:
-    #                 label += 1
-    #             new_labels.append(label)
-    #
-    #     return new_labels
-
     def tokenize_and_align_labels(self, examples):
         tokenized_inputs = self.tokenizer(
             examples[self.hparams.text_column_name],
