0659184373c366393c556f4a97ef60de16f913a3
Zabolotnyi_Artem <zaaabik@gmail.com>
2023-07-23 17:27:22 +0300

    I added experiment with only last layer trained

--- src/models/components/pre_trained_llm.py
+++ src/models/components/pre_trained_llm.py
@@ -20,5 +20,26 @@ class PreTrainedLLM(nn.Module):
         return self.model(**x)
 
 
+class PreTrainedLLMOnlyLastLayer(nn.Module):
+    def __init__(
+            self,
+            model_name: str,
+            num_classes: int = 10,
+    ):
+        super().__init__()
+
+        self.model = AutoModelForTokenClassification.from_pretrained(
+            model_name, num_labels=num_classes,
+            finetuning_task='ner'
+        )
+
+        self.model.roberta.requires_grad_(False)
+        self.model.roberta.encoder.layer[-1].requires_grad_(True)
+        self.model.classifier.requires_grad_(True)
+
+    def forward(self, x):
+        return self.model(**x)
+
+
 if __name__ == "__main__":
     _ = PreTrainedLLM('xlm-roberta-base')
